ed02dadd09e10a266a7294fa0e9f071a935e5a79
Andrei Tkachenia <tkachenia@gmail.com>
2019-10-18 18:41:53 +0300

    Added Lection 8: test

---
+++ Lection/08/lection8.R
@@ -0,0 +1,187 @@
+local_path <- "D:/Ubuntu/Share/Курс_Мультимедийные системы и среды"
+
+setwd(paste0(local_path, "/multimedia_course/Lection/08"))
+Sys.setlocale("LC_CTYPE", "russian")
+source("../wave.R", encoding="utf-8")
+source("../png.R")
+
+lection8.test <- function() {
+   # Oscilogram
+   data_wav <- get.osg("DOOM.wav")
+   writePNG("test_01.png", func = plot.osg, arg_list = list(data_wav, mai = c(2.75, 2.75, 1.25, 1.5)))
+   
+   data <- get.osg("DOOM.wav", from = 29, to = 30, units = "seconds")
+   writePNG("test_02_1.png", func = plot.osg, arg_list = list(data, mai = c(2.75, 2.75, 1.25, 1.5)))
+   writePNG("test_02_2.png", func = plot.osg, arg_list = list(data, type = "l", mai = c(2.75, 2.75, 1.25, 1.5)))
+   writePNG("test_02_3.png", func = plot.osg, arg_list = list(data, xunit = "samples", type = "l", mai = c(2.75, 2.75, 1.25, 1.5)))
+   
+   data <- get.osg("DOOM.wav", from = 10, to = 30, units = "seconds")
+   writePNG("test_03.png", func = plot.osg, arg_list = list(data, mai = c(2.75, 2.75, 1.25, 1.5)))
+   
+   data_mp3 <- get.osg("DOOM.mp3")
+   writePNG("test_04.png", func = plot.osg, arg_list = list(data_mp3, mai = c(2.75, 2.75, 1.25, 1.5)))
+   
+   data <- get.osg("01_16.wav")
+   writePNG("test_05.png", func = plot.osg, arg_list = list(data, mai = c(2.75, 2.75, 1.25, 1.5)))
+   
+   data <- get.osg("01_16.wav", from = 0, to = 2, units = "seconds")
+   writePNG("test_05_1.png", func = plot.osg, arg_list = list(data, type = "l", mai = c(2.75, 2.75, 1.25, 1.5)))
+   
+   #Periodogram
+   data <- get.osg("01_16.wav")
+   prg <- get.prg(data, width = 32768, overlap = -(data@samp.rate - 32768), spec_below = -80)
+   for (i in 1:length(prg@spec)) {
+      name <- paste0("0", i)
+      writePNG(paste0("test_06_", substr(name, nchar(name) - 1, nchar(name)), ".png"), func = plot.prg, arg_list = list(prg, which = i, ylim = c(-120, 0), log = "x"))
+   }
+   writePNG(paste0("test_07_1.png"), func = plot.prg, arg_list = list(prg, which = 1, ylim = c(-120, 0)))
+   writePNG(paste0("test_07_2.png"), func = plot.prg, arg_list = list(prg, which = 1, ylim = c(-120, 0), log = "x"))
+   
+   wav <- get.osg("DOOM.wav")
+   prg <- get.prg(wav, width = 4096, overlap = 1024, spec_below = -80)
+   writePNG("test_08_1.png", func = plot.prg, arg_list = list(prg, which = 45))
+   writePNG("test_08_2.png", func = plot.prg, arg_list = list(prg, which = 45, log = "x"))
+
+   #Spectre
+   wav <- get.osg("DOOM.wav")
+   
+   pws <- get.pws(wav, fs_band = 8000)
+   writePNG("test_09_1.png", func = plot.pws, arg_list = list(pws, wav@samp.rate))
+   
+   pws <- get.pws(wav)
+   writePNG("test_09_2.png", func = plot.pws, arg_list = list(pws, wav@samp.rate))
+   
+   NULL
+}
+
+# Experiment
+lection8.make <- function() {
+     lection8.test()
+   
+     # Pentogram spectre
+     # full
+     data1 <- get.osg("DOOM.wav")
+     writePNG("!_1.png", func = plot.osg, arg_list = list(data1, main = "Осцилограма"))
+     pws1 <- get.pws(data1, 0.025, 0.005, fs_band = 17500, spec_below = -60)
+     writePNG("!_2.png", func = plot.pws, arg_list = list(pws1, main = "Спектр"))
+     # part
+     data2 <- get.osg("DOOM.wav", from = 15.5, to = 17.5, units = "seconds")
+     writePNG("!_3.png", func = plot.osg, arg_list = list(data2, main = "Осцилограма"))
+     pws2 <- get.pws(data2, 0.025, 0.005, fs_band = 17500, spec_below = -60)
+     pws2[pws2 <= -100] <- -500
+     pws2[pws2 > -100 && pws2 <= -65] <- 0
+     pws2[pws2 > -65] <- -500
+     plot.pws(pws2)
+     writePNG("!_4.png", func = plot.pws, arg_list = list(pws2, main = "Спектр"))
+     # Pentogram spectre
+     
+     # Referenced wav
+     # full oscilogram
+     data3 <- get.osg("01_16.wav")
+     writePNG("!_5.png", func = plot.osg, arg_list = list(data3, main = "Осцилограма"))
+     
+     for (i in 0:5) {
+          # part oscilogram
+          data4 <- get.osg("01_16.wav", from = 2*i, to = 2*i + 0.02, units = "seconds")
+          writePNG(paste0("!_", sprintf("%d", 6 + i), "-osg.png"), func = plot.osg, arg_list = list(data4, main = "Осцилограма", draw_ox = F))
+          # part spectre
+          data5 <- get.osg("01_16.wav", from = 2*i, to = 2*i + 1, units = "seconds")
+          prg1 <- get.prg(data5, width = 32768, overlap = -(data5@samp.rate - 32768), spec_below = -80)
+          writePNG.open(paste0("!_", sprintf("%d", 6 + i), "-prg.png"), func = plot.prg, arg_list = list(prg1, which = 1, ylim = c(-120, 0), log = "x"))
+          writePNG.close(func = plot.prg, arg_list = list(prg1, which = 1, ylim = c(-120, 0), log = "x", type = "l", main = "Периодограмма"))
+     }
+     # Referenced wav
+     
+     # Compare mp3
+     # mp3 <- get.osg("03_constant_128.mp3", from = 2258) # constant 128 kbps delta: 10.83% (compression = 5.48 times)
+     # mp3 <- get.osg("04_constant_320.mp3", from = 2258) # constant 320 kbps delta: 5.98% (compression = 2.19 times)
+     # mp3 <- get.osg("05_variable_128.mp3", from = 2258) # variable 128 kbps delta: 14.45% (compression = 9.48 times)
+
+     file_name_list = c("03_constant_128.mp3", "04_constant_320.mp3", "05_variable_128.mp3")
+     description_list = c("constant 128 kbps", "constant 320 kbps", "variable 128 kbps")
+     for (i in 1:length(file_name_list)) {
+          mp3 <- get.osg(file_name_list[i], from = 2258)
+          writePNG(paste0("!_", sprintf("%d", 11 + i), "_osg.png"), func = plot.osg, arg_list = list(mp3, main = paste0("Осцилограма", sprintf(" (%s)", description_list[i]))))
+          
+          data <- data3
+          data@left[1:length(mp3@left)] <- data3@left[1:length(mp3@left)] - mp3@left
+          data@left <- pmax( -32768, pmin(data@left, 32767))
+          writePNG(paste0("!_", sprintf("%d", 11 + i), "-osg.png"), func = plot.osg, arg_list = list(data, main = paste0("Осцилограма искажения", sprintf(" (%s)", description_list[i]))))
+          pws <- get.pws(data, spec_below = -60)
+          writePNG(paste0("!_", sprintf("%d", 11 + i), "-pws.png"), func = plot.pws, arg_list = list(pws, main = paste0("Спектр искажения", sprintf(" (%s)", description_list[i]))))
+          print(paste0(description_list[i], " delta: ", sprintf("%.2f%%", 100.0*sum(abs(as.numeric(data@left)))/sum(abs(as.numeric(data3@left)))), sprintf(" (compression = %.2f times)", file.size("01_16.wav")/file.size(file_name_list[i]))))
+          writeWave(data, paste0("!_", sprintf("%d", 11 + i), "-osg.wav"))
+     }
+     # Compare mp3
+     
+     # Compare flac
+     # flac <- get.osg("07_faster_24.wav") # flac faster 24 bit delta: 0.0313% (compression = 1.41 times)
+     # flac <- get.osg("08_best_24.wav")   # flac best 24 bit delta: 0.0314% (compression = 1.75 times)
+
+     file_name_list = c("07_faster_24.wav", "08_best_24.wav")
+     flac_name_list = c("07_faster_24.flac", "08_best_24.flac")
+     description_list = c("flac faster 24 bit", "flac best 24 bit")
+     for (i in 1:length(file_name_list)) {
+          flac <- get.osg(file_name_list[i])
+          writePNG(paste0("!_", sprintf("%d", 14 + i), "_osg.png"), func = plot.osg, arg_list = list(flac, main = paste0("Осцилограма", sprintf(" (%s)", description_list[i]))))
+          
+          data <- data3
+          data@left[1:length(flac@left)] <- data3@left[1:length(flac@left)] - flac@left
+          data@left <- pmax( -32768, pmin(data@left, 32767))
+          writePNG(paste0("!_", sprintf("%d", 14 + i), "-osg.png"), func = plot.osg, arg_list = list(data, main = paste0("Осцилограма искажения", sprintf(" (%s)", description_list[i]))))
+          pws <- get.pws(data, spec_below = -60)
+          writePNG(paste0("!_", sprintf("%d", 14 + i), "-pws.png"), func = plot.pws, arg_list = list(pws, main = paste0("Спектр искажения", sprintf(" (%s)", description_list[i]))))
+          print(paste0(description_list[i], " delta: ", sprintf("%.4f%%", 100.0*sum(abs(as.numeric(data@left)))/sum(abs(as.numeric(data3@left)))), sprintf(" (compression = %.2f times)", file.size("01_16.wav")/file.size(flac_name_list[i]))))
+          writeWave(data, paste0("!_", sprintf("%d", 14 + i), "-osg.wav"))
+     }
+     # Compare flac
+     
+     # Steganography
+     wav_fn_in <- "01_16.wav"
+     wav_fn_out <- "!01_16.wav"
+     steg_fn_in <- "01_16.7z"
+     steg_fn_out <- "!01_16.7z"
+     
+     data_steg <- get.osg(wav_fn_in)
+     fsize <- file.info(steg_fn_in)$size
+     if (fsize > length(data_steg@left))
+          warning("Wav file is too small")
+     file <- file(steg_fn_in, "rb")
+     
+     pos <- 1
+     for (i in 1:fsize) {
+          raw <- readBin(file, "raw", n = 1)
+          bits <- rawToBits(raw)
+          for (j in 1:length(bits)) {
+               data_steg@left[pos] <- bitwAnd(data_steg@left[pos], bitwNot(1)) # make little bit equal 0
+               if (as.integer(bits[j]) == 1)
+                    data_steg@left[pos] <- data_steg@left[pos] + 1
+               pos <- pos + 1
+          }
+     }
+     close(file)
+     writeWave(data_steg, wav_fn_out)
+     
+     #
+     dataIn <- get.osg(wav_fn_in, units = "samples", from = 1, to = fsize)
+     dataOut <- get.osg(wav_fn_out, units = "samples", from = 1, to = fsize)
+     dataOut@left <- dataIn@left - dataOut@left
+     writePNG("!_17.png", func = plot.osg, arg_list = list(dataOut, main = "Осцилограма искажения (variable 128 kbps)", xunit = "samples"))
+     print(paste0(wav_fn_out, " delta: ", sprintf("%.4f%%", 100.0*sum(abs(as.numeric(dataOut@left)))/sum(abs(as.numeric(dataIn@left)))), sprintf(" (compression = %.2f times)", file.size(wav_fn_in)/file.size(wav_fn_out))))
+     # !01_16.wav delta: 0.0025% (compression = 1.00 times)
+     
+     data_steg <- get.osg(wav_fn_out)
+     file <- file(steg_fn_out, "wb")
+     pos <- 1
+     for (i in 1:fsize) {
+          bits <- raw(len = 8)
+          for (j in 1:8) {
+               bits[j] <- intToBits(data_steg@left[pos])[1]
+               pos <- pos + 1
+          }
+          writeBin(packBits(bits, "raw"), file)
+     }
+     close(file)
+}
+
+lection6.make()
---
+++ Lection/wave.R
@@ -0,0 +1,245 @@
+# install.packages("tuneR")
+library(tuneR)
+library(tools)
+
+g_mai <- c(2.75, 2.75, 2.5, 1.5)
+g_lwd_scale <- 1.5
+g_cex <- 3.0
+g_cex.axis <- 1.0
+g_cex.lab <- 1.25
+g_cex.main <- 1.75
+g_cex.sub <- 0.75
+
+plot.set.par <- function(cex = g_cex, cex.axis = g_cex.axis, cex.lab = g_cex.lab, cex.main = g_cex.main, cex.sub = g_cex.sub, mai = g_mai) {
+   par(cex = cex, cex.axis = cex.axis, cex.lab = cex.lab, cex.main = cex.main, cex.sub = cex.sub, mai = mai)
+}
+
+# Test (not use)
+get <- function(x, ...) {
+     message(123)
+     print(class(x)[1])
+     switch (class(x)[1],
+             character = do.call(get.osg, c(x, list(...))),
+             Wave = do.call(get.prg, c(x, list(...)))
+     )
+}
+
+get_unit_coef <- function(data, units = c("samples", "seconds", "minutes", "hours")) {
+     coef <- switch (units[1],
+                     samples = 1,
+                     seconds = data@samp.rate,
+                     minutes = 60*data@samp.rate,
+                     hours = 3600*data@samp.rate
+     )
+     
+     if (is.null(coef))
+          warning(paste("Wrong units argument -", units[1]))
+     
+     coef
+}
+
+get.osg <- function(filename, from = 1, to = Inf, units = c("samples", "seconds", "minutes", "hours")) {
+     osg <- switch (file_ext(filename),
+                    mp3 = readMP3(filename),
+                    wav = readWave(filename)
+     )
+     
+     if (is.null(osg)) {
+          warning(paste("Unable to read file -", filename))
+          return(NULL)
+     }
+     
+     coef <- get_unit_coef(osg, units)
+     
+     if (!is.null(coef)) {
+          from <- from * coef
+          to <- to * coef
+          len <- length(osg@left)
+          
+          if (from == 0 || from > len)
+               from = 1
+          if (to > len)
+               to = len
+          
+          osg@left <- osg@left[from:to]
+          if (osg@stereo[1])
+               osg@right <- osg@right[from:to]
+     }
+     
+     osg
+}
+
+data.osg.to_mono <- function(data) {
+     mono_data <- data@left
+     if (data@stereo) {
+        mono_data <- mono_data + data@right
+     }
+     
+     mono_data
+}
+
+data.osg.normalize <- function(data) {
+     maximum <- max(abs(range(data)))
+     norm_data <- data / maximum
+     
+     norm_data
+}
+
+data.osg.ranged <- function(data, x_resolution = NULL) {
+     if (is.null(x_resolution)) {
+          return(data)
+     }
+     
+     ranged_data <- rep(0, times = 2*x_resolution)
+     dt <- length(data)/x_resolution
+     for (i in 1:x_resolution) {
+          min_max <- range(data[(i*dt) : ((i+1)*dt)])
+          ranged_data[2*i - 1] <- min_max[1]
+          ranged_data[2*i] <- min_max[2]
+     }  
+     
+     ranged_data
+}
+
+data.osg <- function(osg, x_resolution = NULL) {
+     mono_osg <- data.osg.to_mono(osg)
+     norm_osg <- data.osg.normalize(mono_osg)
+     ranged_osg <- data.osg.ranged(norm_osg, x_resolution)
+
+     ranged_osg
+}
+
+time.osg <- function(osg, x_resolution = 2000, xunit = c("time", "samples")) {
+     dt <- 1
+     t_length <- length(osg@left)
+     if (!is.null(x_resolution)) {
+          dt <- length(osg@left)/x_resolution
+          t_length <- x_resolution
+     }
+     
+     if (xunit[1] == "samples") {
+          t <- seq(0, by = dt, length.out = t_length)
+     } else {
+          t <- seq(0, by = (1/osg@samp.rate)*dt, length.out = t_length)
+     }
+     
+     if (!is.null(x_resolution)) {
+          t <- rep(t, each = 2)
+     }
+     
+     t
+}
+
+plot.osg <- function(osg, x_resolution = 2000, xunit = c("time", "samples"), title = "", xlab = "Врем\u44f", ylab = "Амплитуда", type = "h", draw_ox = TRUE, mai = g_mai) {
+     plot.set.par(mai = mai)
+     
+     y <- data.osg(osg, x_resolution)
+     t <- time.osg(osg, x_resolution, xunit)
+     
+     if (xunit[1] == "samples") {
+          xlab <- paste0(xlab, ", отсчеты")
+     } else {
+          xlab <- paste0(xlab, ", секунды")
+     }
+     
+     plot(t, y, ylim = c(-1, 1), main = title, xlab = xlab, ylab = ylab, type = type, lty = "solid", col = "black", lwd = 1, las = 1)
+     
+     if (draw_ox) {
+          t_head <- head(t, n = 1)
+          t_tail <- tail(t, n = 1)
+          space <- 0.1*(t_tail - t_head)
+          
+          lines(c(t_head - space, t_tail + space), c(0, 0), type = "l", lty = "solid", col = "black", lwd = 1*g_lwd_scale)
+     }
+}
+
+get.prg <- function(data, width = length(data), overlap = 0, postproc = TRUE, spec_below = -40, spec_above = -3) {
+     prg <- periodogram(data, width = width, overlap = overlap)
+     
+     if (postproc) {
+          prg <- data.prg(prg, spec_below = spec_below, spec_above = spec_above)
+     }
+     
+     prg
+}
+
+data.prg.normalize_spec <- function(spec, max = 1, spec_below = -40, spec_above = -3) {
+     spec <- spec/max # normalize magnitude so that max is 0 dB.
+     spec[spec < 10^(spec_below/10)] <- 10^(spec_below/10) # clip below -40 dB.
+     spec[spec > 10^(spec_above/10)] <- 10^(spec_above/10) # clip above -3 dB.
+     
+     spec
+}
+
+data.prg.logarithmic_spec <- function(spec) {
+     spec <- 20*log10(spec)
+     
+     spec
+}
+
+data.prg <- function(prg, spec_below = -40, spec_above = -3) {
+     max <- sapply(prg@spec, max)
+     max <- max(max)
+     prg@spec <- lapply(prg@spec, sapply, data.prg.normalize_spec, max, spec_below, spec_above)
+     prg@spec <- lapply(prg@spec, sapply, data.prg.logarithmic_spec)
+
+     prg
+}
+
+plot.prg <- function(prg, which = 1, ylim = c(min(prg@spec[[which]]), 0), fill = TRUE, xlab = "Частота, Гц", ylab = "Амплитуда, дБ", mai = c(2.75, 2.75, 2.5, 1.5), ...) {
+     plot.set.par(mai = mai)
+     
+     if (which <= 0) {
+          warning(paste("Negative which index -", which))
+          which <- 1
+     }
+     
+     if (which > length(prg@spec)) {
+          warning(paste("Going beyond range of which [1", length(prg@spec), "]  -", which))
+          which <- 1
+     }
+     
+     plot(x = prg@freq, y = prg@spec[[which]], ylim = ylim, type = "l", lty = "solid", col = "black", xlab = xlab, ylab = ylab, ...)
+     
+     # ylab on the right side
+     # args <- c(list(x = substitute(prg@freq), y = substitute(prg@spec[[which]]), ylim = ylim, type = "l", lty = "solid", col = "black", xlab = xlab, ylab = NA), list(...))
+     # do.call(plot, args)
+     # op <- par(mar = c(5,3,4,3) + 0.1)
+     # axis(side = 4, labels = FALSE, tick = FALSE)
+     # mtext(ylab, side = 4, line = 1)
+     # ylab on the right side
+     
+     if (fill) {
+          min <- min(prg@spec[[which]])
+          polygon(c(head(prg@freq, n = 1), prg@freq, tail(prg@freq, n = 1)), c(1.1*ylim[1], prg@spec[[which]], 1.1*ylim[1]), col = "black")
+     }
+}
+
+get.pws <- function(osg, wintime = 0.025, steptime = 0.01, fs_band = osg@samp.rate/2, postproc = TRUE, spec_below = -40, spec_above = -3) {
+     data <- data.osg.to_mono(osg)
+     data <- data.osg.normalize(data)
+   
+     pws <- powspec(data, osg@samp.rate, wintime = wintime, steptime = steptime)
+     
+     pws <- abs(pws[2:(nrow(pws)*fs_band/(osg@samp.rate/2)),]) # magnitude in range (0; fs_band] Hz.
+     
+     if (postproc) {
+          pws <- data.prg.normalize_spec(pws, max(pws), spec_below, spec_above)
+          pws <- data.prg.logarithmic_spec(pws)
+     }
+
+     list(pws = pws, fs_band = fs_band, steptime = steptime)
+}
+
+plot.pws <- function(pws, xlab = "Врем\u44f, секунды", ylab = "Частота, Гц", mai = c(2.75, 2.75, 2.5, 1.5), ...) {
+     plot.set.par(mai = mai)
+     duration <- ncol(pws$pws) * pws$steptime
+
+     image(x = t(pws$pws), col = gray(0:255 / 255), axes = FALSE, xlab = xlab, ylab = NA, ...)
+     mtext(ylab, side = 4, line = 1, cex = g_cex * g_cex.lab)
+
+     axis(side = 2, at = seq(0, 1, 0.1), labels = seq(0, pws$fs_band, pws$fs_band/10), las = 1)
+     
+     labels = round(seq(0, duration, duration/10), digits = 1)
+     axis(side = 1, at = seq(0, 1, 0.1), labels = labels)
+}
